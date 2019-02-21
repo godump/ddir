e0bf2db9d14d4bd44277e7f92af2f31666a06e67
mohanson <mohanson@outlook.com>
2019-02-21 17:30:14 +0800

    2019-02-21 17:30:14

--- README.md
+++ README.md
@@ -5,3 +5,26 @@ Package ddir provides functions for managing the application's data directory.
 ```go
 import "github.com/godump/ddir"
 ```
+
+- [Example](#Example)
+
+# Example
+
+`Base` sets base data path.
+
+```go
+ddir.Base("/tmp/play")
+```
+
+Create `/tmp/play/foo` if it doesn't exists.
+
+```go
+ddir.Make("foo")
+```
+
+Get absolute path `/tmp/play/bar` by given elems:
+
+```go
+name = ddir.Join("bar")
+...
+```
--- go.go
+++ go.go
@@ -9,47 +9,84 @@ import (
 )
 
 var (
-	cBase string
-	cDone bool
+	base string
+	done bool
 )
 
+// Base sets base data path. This should be called from a binary's init or
+// main function. If the base is already set then this will log.Panicln.
 func Base(base string) {
-	if cDone {
-		log.Fatalln("ddir: call Base() multiple times")
+	if done {
+		log.Panicln("ddir: call Base() multiple times")
 	}
-	cBase = base
-	cDone = true
+	base = base
+	done = true
 	Make()
 }
 
-func Auto(elem ...string) {
+// Join joins any number of path elements into a single path, adding
+// a Separator if necessary.
+func Join(elem ...string) string {
+	return filepath.Join(append([]string{base}, elem...)...)
+}
+
+func none(name string) bool {
+	_, err := os.Stat(name)
+	return err != nil && os.IsNotExist(err)
+}
+
+// Make ensures the directory structure. If folder doesn’t exist, create it.
+//
+// Example:
+//   Base("/tmp/")
+//   Make("data0") // make /tmp/data0 if it doesn't exist
+//   Make("data1") // make /tmp/data1 if it doesn't exist
+func Make(elem ...string) {
+	name := Join(elem...)
+	if !none(name) {
+		return
+	}
+	if err := os.Mkdir(name, 0755); err != nil {
+		log.Panicln(err)
+	}
+}
+
+func autoMakeWindows(elem ...string) {
 	seps := []string{}
-	if runtime.GOOS == "windows" {
-		seps = append(seps, os.Getenv("localappdata"))
-		seps = append(seps, elem...)
-		Base(filepath.Join(seps...))
-		return
-	}
-	if runtime.GOOS == "linux" && runtime.GOARCH == "arm" {
-		return
-	}
+	seps = append(seps, os.Getenv("localappdata"))
+	seps = append(seps, elem...)
+	Base(filepath.Join(seps...))
+}
+
+func autoMakeAndroid(elem ...string) {
+}
+
+func autoMakeDefault(elem ...string) {
 	u, err := user.Current()
 	if err != nil {
-		log.Fatalln(err)
+		log.Panicln(err)
 	}
+	seps := []string{}
 	seps = append(seps, u.HomeDir)
 	seps = append(seps, elem...)
 	seps[1] = "." + seps[1]
 	Base(filepath.Join(seps...))
 }
 
-func Join(elem ...string) string {
-	return filepath.Join(append([]string{cBase}, elem...)...)
-}
-
-func Make(elem ...string) {
-	err := os.Mkdir(Join(elem...), 0755)
-	if err != nil && !os.IsExist(err) {
-		log.Fatalln(err)
+// Auto is an automatic Base function call affected by the operating system.
+// Most applications' data directories follow the operating system's
+// specifications, for example, the data directory of vim is placed in ~/.vim.
+//
+// Example:
+//   Auto("play") // Equals with Base("~/AppData/Local/play") on windows
+//   Auto("play") // Equals with Base("~/.play") on linux
+func Auto(elem ...string) {
+	switch {
+	case runtime.GOOS == "windows":
+		autoMakeWindows(elem...)
+	case runtime.GOOS == "linux" && runtime.GOARCH == "arm":
+		autoMakeAndroid(elem...)
+	default:
+		autoMakeDefault(elem...)
 	}
 }
